6c816b2a82424ede61b95f9dc5173ff8f88fdbc0
Kyrylo Davydovskyi <kirill.davidovskiy.work@gmail.com>
2022-08-18 01:19:42 +0300

    add RichestCustomerWealth, moved classes to packages

--- src/main/java/strings/SortCharactersByFrequency.java
+++ src/main/java/strings/SortCharactersByFrequency.java
@@ -1,3 +1,5 @@
+package strings;
+
 import java.util.HashMap;
 
 public class SortCharactersByFrequency {
--- src/test/java/strings/FirstUniqueCharacterInStringTest.java
+++ src/test/java/strings/FirstUniqueCharacterInStringTest.java
@@ -1,7 +1,9 @@
-import org.junit.jupiter.api.Test;
+package strings;
+
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import strings.FirstUniqueCharacterInString;
 
 import java.util.stream.Stream;
 
